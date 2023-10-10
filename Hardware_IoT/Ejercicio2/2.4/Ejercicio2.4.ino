840ab2a0d47c6a2afbf7ff499f77aec2f348ff3e
César Alexis Cruz Campos <88726165+Cesar121101@users.noreply.github.com>
2023-10-10 13:17:34 +0200

    Add files via upload

--- Hardware_IoT/Ejercicio2/2.4/Ejercicio2.4.ino
+++ Hardware_IoT/Ejercicio2/2.4/Ejercicio2.4.ino
@@ -1,22 +1,28 @@
 #include "hardware_D1.h"
+#include "FS.h"
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 
-//const char *ssid  = "MiFibra-C776";
-//const char *password = "auCpN7KN";
-const char *ssid  = "Cesarin";
-const char *password = "cesar121101";
+const char *ssid  = "MiFibra-C776";
+const char *password = "auCpN7KN";
+//const char *ssid  = "DIGIFIBRA-hSbN";
+//const char *password = "StXtC4k6KPdK";
+//const char *ssid  = "Cesarin";
+//const char *password = "cesar121101";
 int medicion_luz = 0;
 int tiempo_actual = 0;
 int tiempo_anterior = 0;
 int umbral;
 bool primer_buzzer = true;
+bool enviarLuminosidad = false;
 String numero_cadena;
 
 ESP8266WebServer server(80);
 
-String webPage;
+String webPage = "";
 
 void paginaNoEncontrada(){
   String message = "File not Found \n\n";
@@ -66,14 +72,11 @@ void setup() {
   Serial.print("Direccion IP: ");
   Serial.println(WiFi.localIP());
 
-  webPage = "<h1 style=\"text-align: center;\">&nbsp;<span style=\"color: #ff0000;\"><strong>Bienvenidos al Servidor</strong></span></h1>";
-  webPage += "<h4 style=\"text-align: center;\"><a href=\"On\">Encender alarma</a></h4><p>&nbsp;</p>";
-  webPage += "<h4 style=\"text-align: center;\"><a href=\"Off\">Apagar alarma</a></h4><br>";
-  webPage += "<form action=\"Num\" method=\"post\"><h4 style=\"text-align: center;\"><input id=\"numero\" name=\"numero\" required=\"\" type=\"number\"";
-  webPage += "placeholder=\"Umbral de luminosidad\" /> </h4>";
-  webPage += "<h4 style=\"text-align: center;\"><input type=\"submit\" value=\"Enviar\" /></h4></form>";
-  webPage += "<form action=\"Luz\" method=\"post\"><h4 style=\"text-align: center;\"><input type=\"submit\" value=\"Luz Actual\" /></h4></form>";
-  webPage += "<h3 style=\"text-align: center;\">Luminosida Actual:</h3>";
+  if(!SPIFFS.begin()){
+    Serial.println("Error");
+    return;
+  }
+  cargarWeb();
   
   server.on("/", [](){
     server.send(200,"text/html", webPage); 
@@ -98,14 +101,26 @@ void setup() {
     //Convertir el dato que recibimos a entero numero.toInt()
     server.send(200,"text/html", webPage); 
   });
-  server.on("/Luz", [](){
+  server.on("/Luz",[](){
     Serial.println("Enviar luminosidad");
     medicion_luz = analogRead(sensor_luz);
     server.send(200,"text/html", webPage + "<h3 style=\"text-align: center;\">" + String(medicion_luz) + "</h3>"); 
-  });
+  }); 
   server.onNotFound(paginaNoEncontrada);
   server.begin();
   Serial.println("HTPP server started"); 
+}
+
+void cargarWeb(){
+  File file = SPIFFS.open("/paginaWeb.html","r");
+  if(!file){
+    Serial.println("No se puede abrir");
+    return;
+  }
+  while(file.available()){
+    webPage += file.readString();
+  }
+  file.close();
 }
 
 void loop() {
