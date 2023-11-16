1ca1da05c80c5ae45ba87cf71266a55a1f2951ad
César Alexis Cruz Campos <88726165+Cesar121101@users.noreply.github.com>
2023-11-16 18:02:21 +0100

    Add files via upload

--- Microprocesadores/Diseño Final/Clock/clock.c
+++ Microprocesadores/Diseño Final/Clock/clock.c
@@ -2,20 +2,18 @@
 #include "stm32f4xx_hal.h"
 #include <stdlib.h>                        // CMSIS RTOS header file
 #include "stdio.h"
+#include "lcd.h"
 
 /*----------------------------------------------------------------------------
  *      Thread 1 'Thread_Name': Sample thread
  *---------------------------------------------------------------------------*/
  
 osThreadId_t clock;                        // thread id 
-osMessageQueueId_t id_MsgQueue;
-extern osThreadId_t Listener;
 
 osTimerId_t timsoft1;
 uint32_t segundos =0;
 uint32_t minutos = 0;
 uint32_t horas = 0;
-
 
 void clock_Func (void *argument);                   // thread function
 void Timer_Callback(void *arg);
@@ -33,7 +31,7 @@ void Timer_Callback(void *arg){
 	}
 }
 
-int Init_ClockThread (void) {
+int init_ClockThread (void) {
 	clock = osThreadNew(clock_Func, NULL, NULL); //Crear el Thread del timer
 	if (clock == NULL) {
     return(-1);
@@ -43,7 +41,7 @@ int Init_ClockThread (void) {
  
 void clock_Func (void *argument) {
 	timsoft1 = osTimerNew(Timer_Callback, osTimerPeriodic, NULL, NULL); //Crear el timer	
-	osTimerStart(timsoft1,1000); //Reiniciar el timer
+	osTimerStart(timsoft1,1000); //Iniciar el timer
   while (1) {
   }
 }
--- Microprocesadores/Diseño Final/Clock/clock.h
+++ Microprocesadores/Diseño Final/Clock/clock.h
@@ -1,4 +1,4 @@
 #ifndef __clock_H
 #define __clock_H
-	int Init_ClockThread(void);
+	int init_ClockThread(void);
 #endif
