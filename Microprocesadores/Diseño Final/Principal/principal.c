11150ea97af7e652be667024e8e9fdd7fab568c3
César Alexis Cruz Campos <88726165+Cesar121101@users.noreply.github.com>
2023-11-19 22:52:43 +0100

    Add files via upload

--- Microprocesadores/Diseño Final/Principal/principal.c
+++ Microprocesadores/Diseño Final/Principal/principal.c
@@ -1,1 +1,42 @@
+#include "cmsis_os2.h"                          // CMSIS RTOS header file
+#include "stm32f4xx_hal.h"
+#include <stdlib.h>                        // CMSIS RTOS header file
+#include "stdio.h"
 
+/*----------------------------------------------------------------------------
+ *      Thread 1 'Thread_Name': Sample thread
+ *---------------------------------------------------------------------------*/
+ 
+osThreadId_t principal;                        // thread id 
+osMessageQueueId_t queue_lcd;
+
+char str[50];
+int i;
+
+void principal_Func (void *argument);                   // thread function
+
+int init_Principal(void) {
+	principal = osThreadNew(principal_Func, NULL, NULL); //Crear el Thread del timer
+	queue_lcd = osMessageQueueNew(64, sizeof(uint8_t), NULL);
+	if (principal == NULL) {
+    return(-1);
+  }
+  return(0);
+}
+
+void principal_Func (void *argument) {
+	int linea1 = 1;
+	int linea2 = 2;
+	sprintf(str,"hola como estas");
+	
+	for(i = 0; i<sizeof(str); i++){
+		if(str[i] != NULL){
+			osMessageQueuePut(queue_lcd, &linea1, 0U, 0U);
+			osMessageQueuePut(queue_lcd, &str[i], 0U, 0U);
+		}
+	}
+	
+
+  while (1) {
+  }
+}
---
+++ Microprocesadores/Diseño Final/Principal/principal.h
@@ -0,0 +1,4 @@
+#ifndef __principal_H
+#define __principal_H
+	int init_Principal(void);
+#endif
