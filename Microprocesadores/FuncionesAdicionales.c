664b6782d6ec8b7f41860ea97adc7857609dd288
César Alexis Cruz Campos <88726165+Cesar121101@users.noreply.github.com>
2023-11-26 13:38:54 +0100

    Update FuncionesAdicionales.c

--- Microprocesadores/FuncionesAdicionales.c
+++ Microprocesadores/FuncionesAdicionales.c
@@ -24,3 +24,44 @@ void diagonales(){
 		contador++;
 	}
 }
+
+//Desplazar izquierda y derecha un numero finito de veces (contador = numero de veces a desplazar y valor es una flag)
+void desplazar(){
+	if(contador < 97 && valor == 0){
+		for(int i = 0; i < 512; i++){
+			buffer2[i] = buffer[i];
+		}
+		for(int i = 0; i < 128; i++){
+			if(i == 0){
+				buffer[i] = buffer2[i+127];
+				buffer[i+128] = buffer2[i+255];
+			}
+			else{
+				buffer[i] = buffer2[i-1];
+				buffer[i+128] = buffer2[i+127];
+			}
+		}
+		contador++;
+	}else{
+		valor = 1;
+		for(int i = 0; i < 512; i++){
+				buffer2[i] = buffer[i];
+		}
+		for(int i = 0; i < 128; i++){
+			if(i == 127){
+				buffer[i] = buffer2[i-127];
+				buffer[i+128] = buffer2[i+1];
+			}
+			else{
+				buffer[i] = buffer2[i+1];
+				buffer[i+128] = buffer2[i+129];
+			}
+		}
+		contador--;
+		if(contador == 9){
+			valor = 0;
+		}
+	}
+	LCD_update();
+}
+
