825171a52511cddb722b8825ae920affc370d44b
César Alexis Cruz Campos <88726165+Cesar121101@users.noreply.github.com>
2023-12-01 00:27:12 +0100

    Add files via upload

--- Microprocesadores/Diseño Final/Principal/principal.c
+++ Microprocesadores/Diseño Final/Principal/principal.c
@@ -2,75 +2,79 @@
 #include "stm32f4xx_hal.h"
 #include <stdlib.h>                        // CMSIS RTOS header file
 #include "stdio.h"
+#include "string.h"
+#include "lcd.h"
+#include "joystick.h"
 
 //Threads IDs
 osThreadId_t principal;                         
-extern osThreadId_t adc;
 
 //Queues IDs
-osMessageQueueId_t queue_lcd;
-extern osMessageQueueId_t queue_adc;
+extern osMessageQueueId_t queue_joystick;
+extern osMessageQueueId_t queue_lcd;
+//extern osMessageQueueId_t queue_adc;
 
 //Variables
 extern int horas,minutos,segundos;
-int i;
-char str[50];
-float pot1, pot2;
+char memoria[10][35];
+msgQueue_LCD mensaje;
 
-void adcFunc(){
-	uint8_t potenciometro;
-	osThreadFlagsSet(adc,0x1);
-	osMessageQueueGet(queue_adc, &potenciometro, NULL, 10U);
-	if(potenciometro == 1){
-		osMessageQueueGet(queue_adc, &pot1, NULL, 10U);
-	}else if(potenciometro == 2){
-		osMessageQueueGet(queue_adc, &pot2, NULL, 10U);
-	}
+void modoReposo(){
+	sprintf(mensaje.cadena,"     SBM 2023");
+	mensaje.linea = 1;
+	mensaje.subrayar = 0;
+	mensaje.numero = 0;
+	osMessageQueuePut(queue_lcd, &mensaje, 0U, 0U);
+	sprintf(mensaje.cadena,"      %02d:%02d:%02d", horas,minutos,segundos);
+	mensaje.linea = 2;
+	mensaje.subrayar = 0;
+	mensaje.numero = 0;
+	osMessageQueuePut(queue_lcd, &mensaje, 0U, 0U);
 }
 
-void horaLCD(){
-	int linea1 = 1;
-	int linea2 = 2;
-	int limpiar = 3;
-	osMessageQueuePut(queue_lcd, &limpiar, 0U, 0U);
-	sprintf(str,"H: %02d M: %02d S:%02d", horas,minutos,segundos);
-	for(i = 0; i<sizeof(str); i++){
-		if(str[i] != NULL){
-			osMessageQueuePut(queue_lcd, &linea1, 0U, 0U);
-			osMessageQueuePut(queue_lcd, &str[i], 0U, 0U);
-		}
+void modoActivo(){
+	for(int i = 0; i < sizeof(mensaje.cadena); i++){
+		mensaje.cadena[i] = ' ';
 	}
-}
-
-void potLCD(){
-	int linea1 = 1;
-	int linea2 = 2;
-	int limpiar = 3;
-	osMessageQueuePut(queue_lcd, &limpiar, 0U, 0U);
-	sprintf(str,"Tm: %04.1f Tr: %04.1f",pot1,pot2);
-	for(i = 0; i<sizeof(str); i++){
-		if(str[i] != NULL){
-			osMessageQueuePut(queue_lcd, &linea1, 0U, 0U);
-			osMessageQueuePut(queue_lcd, &str[i], 0U, 0U);
-		}
+	sprintf(mensaje.cadena,"   ACT---%02d:%02d:%02d---", horas,minutos,segundos);
+	mensaje.linea = 1;
+	mensaje.subrayar = 0;
+	mensaje.numero = 0;
+	osMessageQueuePut(queue_lcd, &mensaje, 0U, 0U);
+	sprintf(mensaje.cadena," Tm:21.5-Tr:22.5-D:80%%");
+	mensaje.linea = 2;
+	mensaje.subrayar = 0;
+	mensaje.numero = 0;
+	osMessageQueuePut(queue_lcd, &mensaje, 0U, 0U);
+	for(int i = 0; i < 9; i++){
+		memcpy(memoria[i],memoria[i + 1], sizeof(memoria[i]));
 	}
+	sprintf(memoria[9],"%02d:%02d:%02d--Tm:XX.X--Tr:YY.Y--D:ZZ%%",horas,minutos,segundos);
 }
 
 void principal_Func (void *argument);                   // thread function
 int init_Principal(void) {
 	principal = osThreadNew(principal_Func, NULL, NULL); //Crear el Thread del timer
-	queue_lcd = osMessageQueueNew(64, sizeof(uint8_t), NULL);
 	if (principal == NULL) {
     return(-1);
   }
   return(0);
 }
 
+uint32_t estado = 0;
 void principal_Func (void *argument) {
-  
+	MSGQUEUE_JOYSTICK joystick_p;
 	while (1) {
-		adcFunc();
-		potLCD();
-		osDelay(500);
+		osMessageQueueGet(queue_joystick, &joystick_p, NULL, 1000);
+		if(joystick_p.joystick_value == 5 && joystick_p.pulsacion == 1){
+			joystick_p.joystick_value = 0;
+			joystick_p.pulsacion = 0;
+			estado = 1;
+		}
+		if(estado == 0){
+			modoReposo();
+		}else if(estado == 1){
+			modoActivo();
+		}
   }
 }
