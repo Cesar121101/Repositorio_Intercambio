af758dff5de739b01c643d99f6d2c06f59c6e002
Ramón Martínez Delgado <69860663+RamonMt2303@users.noreply.github.com>
2023-12-04 17:29:19 +0100

    Add files via upload

--- Microprocesadores/Diseño Final/SensorTemperatura/temperatura.c
+++ Microprocesadores/Diseño Final/SensorTemperatura/temperatura.c
@@ -9,21 +9,27 @@
 osThreadId_t temperatura;
 osMessageQueueId_t queue_temperatura; 
 
-static void init_GPIO(void);
-void init_I2C(void);
-int init_Temperatura(void);
 void THTemperatura (void *argument);                   // thread function
 
 extern ARM_DRIVER_I2C Driver_I2C1;
 ARM_DRIVER_I2C* I2Cdrv = &Driver_I2C1;
 
-static osStatus_t status;
-float temperaturaSensor = 0.0f;
-uint16_t lecturaSensor;
+static volatile uint32_t I2C_Event;
+
+void I2C_SignalEvent (uint32_t event) {
+  /* Save received events */
+  I2C_Event |= event;
+ 
+  if (event & ARM_I2C_EVENT_TRANSFER_DONE) {
+    /* Transfer or receive is finished */
+		osThreadFlagsSet(temperatura, 0x1);
+		osThreadFlagsSet(temperatura, 0x2);
+  }
+}
 
 void init_I2C(void){
 	/* Initialize the I2C driver */
-	I2Cdrv->Initialize(NULL);
+	I2Cdrv->Initialize(I2C_SignalEvent);
 	/* Power up the I2C peripheral */
 	I2Cdrv->PowerControl(ARM_POWER_FULL);
 	I2Cdrv->Control(ARM_I2C_BUS_SPEED, ARM_I2C_BUS_SPEED_STANDARD);
@@ -46,41 +52,34 @@ static void init_GPIO(void) {
   // Configurar el pin PB9 como SDA
   GPIO_InitStruct.Pin = GPIO_PIN_9;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-	
-	//led de prueba
-  GPIO_InitStruct.Pin = GPIO_PIN_7;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 } 
 
 int init_Temperatura(void) {
-	queue_temperatura = osMessageQueueNew(16, sizeof(float), NULL);
   temperatura = osThreadNew(THTemperatura, NULL, NULL);
   if (temperatura == NULL) {
     return(-1);
   }
+	queue_temperatura = osMessageQueueNew(8, sizeof(float), NULL);
   return (0);
 }
 
 void THTemperatura(void *argument) {
   // Configurar e inicializar el sensor LM75B
-	init_GPIO();
 	init_I2C();
+	float temperaturaSensor = 0.0f;
+	uint16_t lecturaSensor;
 	
   while (1) {
     // Leer la temperatura del sensor LM75B
-		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_7);
 		// Seleccionar el registro que contiene la temperatura
 		uint8_t regAddress = 0;
 		I2Cdrv->MasterTransmit(LM75B_ADDRESS, &regAddress, 1, true);
-		while (I2Cdrv->GetStatus().busy);
+		osThreadFlagsWait(0x1, osFlagsWaitAny, osWaitForever);
 
 		// Leer los datos desde el registro seleccionado
 		uint8_t data[2];
 		I2Cdrv->MasterReceive(LM75B_ADDRESS, data, 2, false);
-		while (I2Cdrv->GetStatus().busy);
+		osThreadFlagsWait(0x2, osFlagsWaitAny, osWaitForever);
 		// Combinar los dos bytes para formar el valor de temperatura completo (11 bits)
 		lecturaSensor = ((data[0] << 8) | data[1]) >> 5;
 
@@ -91,9 +90,25 @@ void THTemperatura(void *argument) {
 		 
 		temperaturaSensor = lecturaSensor * 0.125;
 		
-		status = osMessageQueuePut(queue_temperatura, &temperaturaSensor, 0U, 0U);
-
-    // Esperar un tiempo antes de la próxima lectura
-    osDelay(1000); // Esperar 1 segundo
+		osMessageQueuePut(queue_temperatura, &temperaturaSensor, 0U, 0U);
   }
+}
+
+void THTemperatura_test (void *argument);                   // thread function
+
+float temperaturaSensor_test = 0.0f;
+
+int init_Temperatura_test(void) {
+	osThreadId_t temperatura_test = osThreadNew(THTemperatura_test, NULL, NULL);
+  if (temperatura == NULL) {
+    return(-1);
+  }
+  return (0);
+}
+
+
+void THTemperatura_test(void *argument) {
+  while (1) {
+    osMessageQueueGet(queue_temperatura, &temperaturaSensor_test, NULL, osWaitForever);
+	}
 }
--- Microprocesadores/Diseño Final/SensorTemperatura/temperatura.h
+++ Microprocesadores/Diseño Final/SensorTemperatura/temperatura.h
@@ -1,4 +1,5 @@
 #ifndef __TEMPERATURA_H
 #define __TEMPERATURA_H
 	int init_Temperatura (void);
+	int init_Temperatura_test (void);
 #endif
